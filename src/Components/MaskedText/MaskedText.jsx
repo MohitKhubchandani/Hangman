e8bb654f5fa6d7b3a5c566e7e9a7f47cdf89bad5
Mohit04 <mohitkhubchandani88@gmail.com>
2024-08-26 02:02:13 +0530

    Added guess Letter Logic

---
+++ src/Components/MaskedText/MaskedText.jsx
@@ -0,0 +1,19 @@
+function MaskedText({text, usedLetters}){
+  return(
+    <>
+     
+    </>
+  )
+};
+
+export function getAllCharacters(word, usedLetters){
+  usedLetters = usedLetters.map(letter => letter.toUpperCase());
+  let guessedLetters = new Set(usedLetters);
+  const characters = word.toUpperCase().split('').map(char => {
+    if(guessedLetters.has(char)){
+      return char
+    }
+    return '_'
+  })
+  return characters.join();
+};
